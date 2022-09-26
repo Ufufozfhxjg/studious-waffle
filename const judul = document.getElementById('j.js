323eb031c687750a639f53fa9975490bd18cb132
Ufufozfhxjg <108488438+Ufufozfhxjg@users.noreply.github.com>
2022-09-26 08:23:21 +0700

    Create const judul = document.getElementById('j.js

---
+++ const judul = document.getElementById('j.js
@@ -0,0 +1,13 @@
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'aldi'
+
+const p= document.getElementsByTagName(p);
+p.style.backgroundColor = 'lightblue';
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontsize = '50px';
+
+const p1 = document.getElementsByClassName('p');
+p1[0].innerHTML = 'ini di ubah dari javascript';
